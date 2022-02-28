092d285d264bc829740938b878b8eaa7407491b3
fernandagil <fernandagilgonzalez@gmail.com>
2022-02-28 20:14:00 +0000

    Update function to retrieve one name

--- src/App.js
+++ src/App.js
@@ -3,49 +3,56 @@ import List from './List';
 import axios from 'axios';
 
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
 
-        this.state = {
-            people: [],
-        }
+//         this.state = {
+//             people: [],
+//         }
 
-        this.getPeople = this.getPeople.bind(this);
-    }
+//         this.getPeople = this.getPeople.bind(this);
+//     }
 
-    getPeople() {
-        return axios.get('https://swapi.dev/api/people')
-        .then((response) => {
-            console.log(response.data.results);
-            this.setState( { people: response.data.results } );
-        });
-    }
+//     getPeople() {
+//         return axios.get('https://swapi.dev/api/people')
+//         .then((response) => {
+//             console.log(response.data.results);
+//             this.setState( { people: response.data.results } );
+//         });
+//     }
 
-    componentDidMount() {
-        this.getPeople()
-    }
+//     componentDidMount() {
+//         this.getPeople()
+//     }
 
 
-    render() {
-        const {people} = this.state;
-        return (
-            <div className='App'>
-                <List people={people} />
-            </div>
-        );
-    }
-}
+//     render() {
+//         const {people} = this.state;
+//         return (
+//             <div className='App'>
+//                 <List people={people} />
+//             </div>
+//         );
+//     }
+// }
 
-export default App;
+// export default App;
 
 
 // search people
 
-let button = document.querySelector('#button')
+let button = document.querySelector('#button');
+let name = document.querySelector('#name');
 
-function sayHello() {
-    console.log('hello')
+function getInfo() {
+    axios.get('https://swapi.dev/api/people/1').then(function(response){
+        updateInfo(response.data);
+    })
 }
 
-button.addEventListener('click', sayHello)
+function updateInfo(data) {
+    name.innerText = data.name
+}
+
+button.addEventListener('click', getInfo);
